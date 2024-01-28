7e7763b5c76682e40002a98a047741b09b6fa0e1
[Kani] <[thies.boll@gmail.com]>
2024-01-28 04:09:56 +0100

    more utility
    - fixes z-fighting floor/walls room 0
    - edited lighting in room 1
    - fixed floor z-fighting room 1 + sky
    - fixes z-fighting ceiling room 2 + sky
    - lighting + sky in room 3
    - enhances end level A LOT
    - installs polybrush
    - outcard prototype
    - adds prototype for main menu (scene the player will see first when entering the game... prior to room0)

---
+++ Assets/Scripts/MenuActions.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;  // Import this namespace to use UI elements
+
+public class MenuActions : MonoBehaviour
+{
+    public GameObject creditsText;  // Assign in Inspector
+    public GameObject backButton;   // Assign in Inspector
+    public Button startButton;      // Assign in Inspector
+    public Button creditsButton;    // Assign in Inspector
+    public Button exitButton;       // Assign in Inspector
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Room_0"); // Make sure the scene name matches exactly
+    }
+
+    public void ShowCredits()
+    {
+        // Activate the credits text and back button, deactivate other menu buttons
+        creditsText.SetActive(true);
+        backButton.SetActive(true);
+        startButton.gameObject.SetActive(false);
+        creditsButton.gameObject.SetActive(false);
+        exitButton.gameObject.SetActive(false);
+    }
+
+    public void BackToMenu()
+    {
+        // Deactivate the credits text and back button, reactivate other menu buttons
+        creditsText.SetActive(false);
+        backButton.SetActive(false);
+        startButton.gameObject.SetActive(true);
+        creditsButton.gameObject.SetActive(true);
+        exitButton.gameObject.SetActive(true);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;  // This line is for exiting Play Mode in the Unity Editor
+#endif
+    }
+}
